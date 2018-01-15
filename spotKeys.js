d9e5532ded2d6181a475041ce1d72db8361a27a3
CalebHarsh <caleb.harsh139@gmail.com>
2018-01-14 18:09:22 -0700

    testing

--- spotKeys.js
+++
@@ -1,9 +0,0 @@
-var Spotify = require("node-spotify-api")
-console.log("Spotify is loaded.")
-
-var spotData = new Spotify({
-  id: "2c3c3a58d51b4519975b5fb8d591a2f1",
-  secret: "03f09a92d3504f63a688462d3a2189da"
-})
-
-module.exports = spotData
